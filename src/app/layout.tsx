f97cebc9c94d8f53d5f589eafb9ad3b9b560a477
cinori <chirarurara@gmail.com>
2024-05-22 11:41:16 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,8 +56,8 @@ export default function RootLayout({
         <Link href="https://vndb.org/v6700" target="_blank">
           <div className="font-extralight text-xs mt-2 hover:underline hover:text-cyan-200 text-center">
             <p className="italic">
-              "I've got tons of free time, but not that much. I've got anime to
-              watch, not to mention online auctions I've gotta keep my eye on!"
+              &quot;I&apos;ve got tons of free time, but not that much. I&apos;ve got anime to
+              watch, not to mention online auctions I&apos;ve gotta keep my eye on!&quot;
             </p>
             <p>- すぴぱら (minori)</p>
           </div>
