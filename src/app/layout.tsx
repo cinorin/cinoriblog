31f7c786c7f48b999ec3cc5831a7622bf5632ffb
cinori <chirarurara@gmail.com>
2024-06-17 16:50:44 +0700

    fix: font2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,15 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-// export const inter = Inter({ subsets: ["latin"] });
-
-export const noto = Noto_Sans_JP({
+// Import the font
+const NotoSans = Noto_Sans_JP({
   subsets: ['latin']
-})
+});
 
 export const metadata: Metadata = {
   title: "cinori blog",
-  description: "lorem ipsum",
-  openGraph: {
-    images: '/marusiru.png'
-  }
+  description: "wow cool description"
 };
-
 
 export default function RootLayout({
   children,
@@ -26,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={noto.className}>
+      <body className={NotoSans.className}>
         <div className="mx-auto px-3 max-w-2xl">
           <Navbar />
           {children}
